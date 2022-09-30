100c7f58b344e6df82b36a3c73eec27411794cb2
ashvin1432 <71180235+ashvin1432@users.noreply.github.com>
2022-09-30 21:27:05 +0530

    Ashvin
    Login OTP verification ui complete

--- lib/view/screens/auth/login.dart
+++ lib/view/screens/auth/login.dart
@@ -1,40 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:bl_dairy_app/view/constants/Theme.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class login extends StatelessWidget {
+  const login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          
-          children: [
-            Container(
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(color: MyColors.primary),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CircleAvatar(backgroundColor: Colors.indigo,),
-                  Column(
-                    children: [
-                      Text("Welcome"),
-                      Text("Login to autobackup...")
-                    ],
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Color.fromARGB(99, 255, 255, 255),
+                    ),
+                    padding: EdgeInsets.all(0),
+                    height: 60,
+                    width: 60,
+                    child: Center(
+                      child: Icon(
+                        FeatherIcons.logIn,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        'login to auto backup your data securly.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      )
+                    ],
+                  )
                 ],
               ),
             ),
-
-            SizedBox(width: 400,
-              child: TextField(),
-            ),
-
-
-            ElevatedButton(onPressed: (){}, child: Text("Send otp"))
-          ],
+          ),
         ),
       ),
+      body: Center(
+          child: Container(
+        padding: EdgeInsets.fromLTRB(30, 20, 30, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              cursorColor: MyColors.primary,
+              maxLength: 10,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                suffixIcon: Icon(
+                  FeatherIcons.phone,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                // Respond to button press
+              },
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(50),
+                  backgroundColor: MyColors.primary),
+              icon: Icon(FeatherIcons.send),
+              label: Text("Send OTP"),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
