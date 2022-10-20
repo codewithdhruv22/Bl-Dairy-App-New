8ec5ae93ea088215445e3c30e004fce6ccf5089c
Ashvin Choudhry <ashvin4615@gmail.com>
2022-10-20 23:34:03 +0530

    ui improved

--- lib/view/screens/RmPruchase.dart
+++ lib/view/screens/RmPruchase.dart
@@ -37,40 +37,68 @@ class _RmPurchaseScreenState extends State<RmPurchaseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-body : Column(
-  children: [
-    TextFieldSearch(
-      initialList: searchList,
-      itemsInView: 10,
-      label: 'Product Name',
-      controller: RmNameController,
-      decoration: const InputDecoration(
-        labelText: 'Product Name',
-        border: OutlineInputBorder(),
-      ),
-    ),
-
-    TextField(
-      controller: PricingController,
-      decoration: InputDecoration(
-        hintText: "Pricing/Unit"
-      ),
-    ),
-    TextField(
-      controller: QtyController,
-      decoration: InputDecoration(
-          hintText: "Quantity"
-      ),
-    ),
-
-    ElevatedButton(onPressed: ()async{
-      await RmController.PurhcaseRm(RmModel(RmName: RmNameController.text, RmPrice: double.parse(PricingController.text), RmQty: double.parse(QtyController.text) , purchaseTime: Timestamp.now())).then((_){
-        Get.snackbar("Raw Material Purchase Successful", "Price Updated and Entry Added To Database");
-      });
-    }, child:Text("Purhcase Raw Material"))
-  ],
-)
-
-    );
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+          child: Column(
+            children: [
+              TextFieldSearch(
+                initialList: searchList,
+                itemsInView: 10,
+                label: 'Product Name',
+                controller: RmNameController,
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  labelStyle: TextStyle(fontSize: 14),
+                  labelText: 'Product Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: PricingController,
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  labelStyle: TextStyle(fontSize: 14),
+                  labelText: 'Rate',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                controller: QtyController,
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  labelStyle: TextStyle(fontSize: 14),
+                  labelText: 'Quantity',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    await RmController.PurhcaseRm(RmModel(
+                            RmName: RmNameController.text,
+                            RmPrice: double.parse(PricingController.text),
+                            RmQty: double.parse(QtyController.text),
+                            purchaseTime: Timestamp.now()))
+                        .then((_) {
+                      Get.snackbar("Raw Material Purchase Successful",
+                          "Price Updated and Entry Added To Database");
+                    });
+                  },
+                  child: const Text("Purhcase Raw Material"))
+            ],
+          ),
+        ));
   }
 }
--- lib/view/screens/payment.dart
+++ lib/view/screens/payment.dart
@@ -14,7 +14,6 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
-
   TextEditingController amountController = TextEditingController();
   TextEditingController suppNameEdCont = TextEditingController();
 
@@ -22,52 +21,71 @@ class _PaymentScreenState extends State<PaymentScreen> {
 
   @override
   void initState() {
-
     super.initState();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body : Column(
-        children: [
-          DropdownSearch<MilkSupplierModel>(
-            asyncItems: (String filter) =>
-                milkSupplierController.fetchOneLedger(filter),
-            itemAsString: (MilkSupplierModel u) => u.Name,
-            onChanged: (MilkSupplierModel? data) {
-              setState(() {
-                print("FAT RATE IS HERE");
-                print(data!.Name);
-                print(data.FatRate);
-                suppNameEdCont.text = data.Name;
-
-              });
-            },
-            dropdownDecoratorProps: const DropDownDecoratorProps(
-              dropdownSearchDecoration:
-              InputDecoration(labelText: "Select Supplier"),
-            ),
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+          child: Column(
+            children: [
+              DropdownSearch<MilkSupplierModel>(
+                asyncItems: (String filter) =>
+                    milkSupplierController.fetchOneLedger(filter),
+                itemAsString: (MilkSupplierModel u) => u.Name,
+                onChanged: (MilkSupplierModel? data) {
+                  setState(() {
+                    print("FAT RATE IS HERE");
+                    print(data!.Name);
+                    print(data.FatRate);
+                    suppNameEdCont.text = data.Name;
+                  });
+                },
+                dropdownDecoratorProps: const DropDownDecoratorProps(
+                  dropdownSearchDecoration: InputDecoration(
+                    labelText: "Select Supplier",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Fill Amount';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                  labelStyle: TextStyle(fontSize: 14),
+                  labelText: 'Paid Amount',
+                  border: OutlineInputBorder(),
+                ),
+                controller: amountController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    await PaymentController.addPayment(PaymentModel(
+                            SupplierName: suppNameEdCont.text,
+                            PaidAmount: double.parse(amountController.text)))
+                        .then((_) {
+                      Get.snackbar("ENTRY ADDED SUCCESSFULLY",
+                          "YOUR PAYMENT IS SUCCESSFULLY ADDED TO DATABASE");
+                    });
+                  },
+                  child: const Text("Add Entry"))
+            ],
           ),
-
-
-
-          TextField(
-            controller: amountController,
-            decoration: InputDecoration(
-              hintText: "Paid Amount"
-            ),
-          ),
-          ElevatedButton(onPressed: ()async{
-            await PaymentController.addPayment(PaymentModel(SupplierName:   suppNameEdCont.text, PaidAmount: double.parse(amountController.text))).then((_){
-              Get.snackbar("ENTRY ADDED SUCCESSFULLY", "YOUR PAYMENT IS SUCCESSFULLY ADDED TO DATABASE");
-            });
-
-          }, child: Text("Add Entry"))
-        ],
-      )
-    );
+        ));
   }
 }
